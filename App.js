783afb58432ddb0935ebf37f66a4c3ae98013dff
Artem <artem@edio.school>
2023-01-10 23:09:07 +0200

    Task is done

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Search from './components/search';
+export default function App() {
+  return (
+    <View>
+        <Search></Search>
+    </View>
+  );
+}
+
+//const styles = StyleSheet.create({
+//  container: {
+//    flex: 1,
+//    backgroundColor: 'red',
+//    alignItems: 'center',
+//    justifyContent: 'center',
+//  },
+//});
